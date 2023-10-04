8d564e2d68964fc4c7287982576134e022fe537f
rainman <c@a.com>
2023-10-04 11:52:07 +0200

    Updated README with note about typo in PM2.5 tag string Added command line option to set baud rate Added command line option to set slave address Added command line option to set name of line protocol measurement field Added command line option for dry run (running without submitting data to telegraf) Added command line option to keep storing PM2.5 values with PM2.4 tag for backward compatibility. Use --pm24 to get original behaviour. Added command line option to store different sensor values as fields instead of tags
    Modified command line option for verbosity to have several levels which translate to different log levels. Default behaviour is to not print telegraf string for every submit now. Use -v once to get back original behaviour
    
    Removed old sps30_telegraf.py

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Reads sensirion sps30 sensor via UART interface and posts results as influx line
 Depends on https://pypi.org/project/sensirion-shdlc-driver/
 
 ### Known issues:
+- There is a typo that stores PM2.5 values as PM2.4. Unfortunately if you have influxdb as a backend data store it is not trivial to rename those old values. For backward compatibility there is an option to continue storing it like that but the new default will be to store as PM2.5.
 - Sensor library might report "unknown error 67" on startup. This error means that the current command (probably wakeup or start fan) can not be executed in the current state. This is most likely because a previous run of the program or other program left the sensor in running state and it's already active. The process will continue to run and try to read the sensor anyway. 
 - The sensirion library does not understand "error 67" even though it's documented in the sensor data sheet. 
+  
--- sps30_telegraf_monitor.py
+++ sps30_telegraf_monitor.py
@@ -7,14 +7,13 @@ import time
 import struct
 
 import logging
-import socket
 import requests
 
 from optparse import OptionParser
 import traceback
 
 log = logging.getLogger('SPS30_Sensor')
-SENSOR_VALUES = ['PM1.0', 'PM2.4', 'PM4.0', 'PM10', 'N_PM0.5', 'N_PM1.0', 'N_PM2.5', 'N_PM4.0', 'N_PM10', 'avg_size']
+SENSOR_VALUES = ['PM1.0', 'PM2.5', 'PM4.0', 'PM10', 'N_PM0.5', 'N_PM1.0', 'N_PM2.5', 'N_PM4.0', 'N_PM10', 'avg_size']
 
 
 class ShdlcCmdStart(ShdlcCommand):
@@ -43,7 +42,7 @@ class ShdlcCmdSleep(ShdlcCommand):
 
 
 class ShdlcCmdWake(ShdlcCommand):
-  def __init__(self, device):
+  def __init__(self, device: ShdlcDevice):
     port = device.connection.port
     port._serial.write(0xff)
     super(ShdlcCmdWake, self).__init__(0x11, data=[], max_response_time=0.5, max_response_length=0, post_processing_time=2.0)
@@ -51,22 +50,38 @@ class ShdlcCmdWake(ShdlcCommand):
 
 def parse_options():
   parser = OptionParser()
-  parser.add_option('-v', '--verbose', dest='verbose', action='store_true', default=False)
+  parser.add_option('-v', '--verbose', dest='verbose', action='count', help='Log verbosity. use -vvv for more logging')
   parser.add_option('-n', '--num_mess', dest='number_mess', type='int', help='Number of measurements to perform and average', default=20)
   parser.add_option('-s', '--sleep_int', dest='sleep_int', type='float', help='Time to wait between measurement iterations', default=1)
   parser.add_option('-w', '--warmup_time', dest='warmup_time', type='float', help='Time to wait for fan to spin up and start measurements', default=20)
   parser.add_option('-p', '--port', dest='port', type='string', help='path to serial port io file', default='/dev/ttyAMA0')
-  parser.add_option('-l', '--location', dest='location', type='string', help='location of the sensor', default='UnderStairs')
-  parser.add_option('-t', '--telegraf_address', dest='telegraf', type='string', help='URL for telegraf instance', default='http://192.168.178.220:8090/telegraf')
+  parser.add_option('-l', '--location', dest='location', type='string', help='location tag of the sensor', default='UnderStairs')
   parser.add_option('-i', '--interval', dest='interval', type='int', help='Seconds between waking up and capturing data', default='300')
+  parser.add_option('-b', '--baud_rate', dest='baud_rate', type='int', help='Serial communications port baud rate. Default: 115200', default=115200)
+  parser.add_option('-a', '--slave_address', dest='slave_address', type='int', help='Sensor slave address. Default: 0', default=0)
+  parser.add_option('-m', '--measurement', dest='measurement', type='string', help='The measurement string used to submit to telegraf', default='sps')
+  parser.add_option('-d', '--dry_run', dest='dry_run', type='bool', action='store_true', default='false', help='Read sensor but do not actually send data to telegraf')
+  parser.add_option('--pm24', dest='pm_24', type='bool', action='store_true', default=False, help='Backward compatibility: Store PM2.5 values with PM2.4 tag')
+  parser.add_option('-t', '--telegraf_address', dest='telegraf', type='string', help='URL for telegraf instance. default: "http://192.168.178.220:8090/telegraf"',
+                    default='http://192.168.178.220:8090/telegraf')
+  parser.add_option('--pm_as_field', dest='pm_as_field', action='store_true', default=False,
+                    help='If set, will submit all sensor values as one record. Values will be stored as fields named PMXX or NPM_XX.'
+                         'If not set, every sensor value will be submitted as individual record with field named "value" and additional tag called"pm"')
+  options, args = parser.parse_args()
 
-  return parser.parse_args()
+  if hasattr(options, 'verbose'):
+    if options.verbose == 1:
+      log.setLevel(logging.INFO)
+    if options.verbose == 2:
+      log.setLevel(logging.DEBUG)
+
+  return options, args
 
 
 def readData(options):
   averages = [0] * 10
-  with ShdlcSerialPort(port=options.port, baudrate=115200) as port:
-    device = ShdlcDevice(ShdlcConnection(port), slave_address=0)
+  with ShdlcSerialPort(port=options.port, baudrate=options.baud_rate) as port:
+    device = ShdlcDevice(ShdlcConnection(port), slave_address=options.slave_address)
     log.debug('Waking device')
     try:
       device.execute(ShdlcCmdWake(device))
@@ -74,18 +89,19 @@ def readData(options):
       log.warning('Something went wrong while waking up the sensor')
       log.warning(traceback.format_exc())
     sensor_id = device.get_serial_number()
-    if options.verbose:
-      log.debug('Version')
-      log.debug("Version: {}".format(device.get_version()))
-      log.debug("Product Name: {}".format(device.get_product_name()))
-      log.debug("Article Code: {}".format(device.get_article_code()))
-      log.debug("Serial Number: {}".format(sensor_id))
+
+    if hasattr(options, 'verbose') and options.verbose == 3:
+      log.debug(f'Version: {device.get_version()}')
+      log.debug(f'Product Name: {device.get_product_name()}')
+      log.debug(f'Article Code: {device.get_article_code()}')
+      log.debug(f'Serial Number: {sensor_id}')
 
     log.debug('Starting up fan')
     try:
       device.execute(ShdlcCmdStart())
     except:
       log.error('Failed to start up fan')
+      log.error(traceback.format_exc())
 
     log.debug('Warmup')
     time.sleep(options.warmup_time)
@@ -124,13 +140,25 @@ def upload_telegraf(options, sensor_id, data):
   session = requests.session()
   session.trust_env = False
 
-  for mess, d in zip(SENSOR_VALUES, data):
-    telegraf_string = 'sps30,location=%s,sensor_id=%s,pm=%s value=%f %d' % (options.location, sensor_id, mess, d, timestamp)
-    print(telegraf_string)
+  if options.pm_as_field:
+    data = [str(data)]
+    fields = ','.join(['='.join(x) for x in zip(SENSOR_VALUES, data)])
+    telegraf_string = f'{options.measurement},location={options.location},sensor_id={sensor_id} {fields} {timestamp}'
+    send_telegraf_request(options, session, telegraf_string)
+  else:
+    for mess, d in zip(SENSOR_VALUES, data):
+      # telegraf_string = 'sps30,location=%s,sensor_id=%s,pm=%s value=%f %d' % (options.location, sensor_id, mess, d, timestamp)
+      telegraf_string = f'{options.measurement},location={options.location},sensor_id={sensor_id},pm={mess} value={d} {timestamp}'
+      send_telegraf_request(options, session, telegraf_string)
+
+
+def send_telegraf_request(options, session, telegraf_string):
+  log.info(telegraf_string)
+  if not options.dry_run:
     try:
       response = session.post(options.telegraf, data=telegraf_string)
       log.debug('http response code   : %s' % response.status_code)
-      log.debug('http response heaers : %s' % response.headers)
+      log.debug('http response headers: %s' % response.headers)
       log.debug('http response content: %s' % response.content)
     except:
       log.error('Failed to submit data string %s' % telegraf_string)
@@ -141,9 +169,9 @@ def main_loop(options):
   while True:
     before = time.time()
     sensor_id, data = readData(options)
+    log.debug(sensor_id, data)
     upload_telegraf(options, sensor_id, data)
-    if options.verbose:
-      print(sensor_id, data)
+
     after = time.time()
     diff = options.interval - (after - before)
     if diff > 0:
@@ -152,6 +180,12 @@ def main_loop(options):
 
 def main():
   options, args = parse_options()
+
+  # This option is purely for backward compatibility with existing database schemas which have erroneously been created
+  # with a column called PM2.4 instead of PM2.5
+  if options.pm_24:
+    SENSOR_VALUES[1] = 'PM2.4'
+
   main_loop(options)
 
 
